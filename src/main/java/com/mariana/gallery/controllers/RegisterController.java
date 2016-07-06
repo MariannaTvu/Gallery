20728809051bebf4e046ab4e061666bc24cbf312
Maryana <nico.tvu@gmail.com>
2016-07-06 14:31:08 +0300

    add autologin

--- src/main/java/com/mariana/gallery/controllers/RegisterController.java
+++ src/main/java/com/mariana/gallery/controllers/RegisterController.java
@@ -5,10 +5,18 @@ import com.mariana.gallery.service.gallery.GalleryService;
 import com.mariana.gallery.persistence.user.User;
 import com.mariana.gallery.persistence.user.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +34,9 @@ public class RegisterController {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String registration(Model model) {
         model.addAttribute("user", new User());
@@ -38,10 +49,20 @@ public class RegisterController {
         String pass = passwordEncoder.encode(userForm.getPassword());
         userForm.setPassword(pass);
         userForm.setRole(UserRole.USER);
-
         userDAO.saveUser(userForm);
+        authenticateUser(userForm);
 
-        //  securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
-        return "redirect:/index";
+        return "redirect:/artist_gallery";
+    }
+
+    public void authenticateUser(User user) {
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getLogin());
+
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(
+                        user.getLogin(),
+                        user.getPassword(),
+        userDetails.getAuthorities()));
     }
 }
