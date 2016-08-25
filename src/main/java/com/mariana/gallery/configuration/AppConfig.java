adb7371d1feb007d974c6c1f69bdcce91380ecaf
Maryana <nico.tvu@gmail.com>
2016-08-25 07:25:37 +0300

    add direct db url

--- src/main/java/com/mariana/gallery/configuration/AppConfig.java
+++ src/main/java/com/mariana/gallery/configuration/AppConfig.java
@@ -79,16 +79,21 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     public BasicDataSource dataSource() throws URISyntaxException, URI.MalformedURIException {
        // URI dbUri = new URI(System.getenv("mysql://beee8d71b0625f:d9719262@eu-cdbr-west-01.cleardb.com/heroku_4cfb82943020c35"));
         URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-
-        String username = System.getenv("JDBC_DATABASE_USERNAME");
-        String password = System.getenv("JDBC_DATABASE_PASSWORD");
         String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-     //   String dbUrl = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+        if (dbUri.getUserinfo() != null) {
+            String username = System.getenv("JDBC_DATABASE_USERNAME");
+            String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+            basicDataSource.setUsername(username);
+            basicDataSource.setPassword(password);
+        }
+     //   String dbUrl = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+
+basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
 
         return basicDataSource;
 
